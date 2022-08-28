880b081984a982c9df1fa2c843caa0b4a963a26b
Rohithgv0211 <109509289+Rohithgv0211@users.noreply.github.com>
2022-08-28 19:45:59 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,14 @@
+class area
+  {
+    public static void main(String[]args)
+    {
+      int r=10;
+      double pi=3.14;
+      double ar;
+
+      System.out.println("the area of the circle is");
+      ar=pi*r*r;
+      System.out.println(ar);
+      
+    }                       
+  }
